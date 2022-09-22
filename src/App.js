a3ee9dff496563de28a2df6d9a512c5ed23604c2
ChimwemweG <gwamba.stargate@gmail.com>
2022-09-22 17:25:22 +0200

    added footer, contact and sale points sections

--- src/App.js
+++ src/App.js
@@ -3,17 +3,32 @@ import Nav from './components/nav/nav';
 import Banner from './components/banner/banner';
 import Services from './components/services/services';
 import SalesPoint from './components/salesPoint/salesPoint';
+import Contact from './components/contact/contact';
+import Footer from './components/footer/footer';
+
 
 
 
 function App() {
   return (
     <div>
+      <div className='bannerSectionContainer'>
+        <div className='container'>
+          <Nav></Nav>
+          <Banner></Banner> 
+        </div>
+      </div>
+
       <div className='container'>
-        <Nav></Nav>
-        <Banner></Banner> 
         <Services></Services>
         <SalesPoint></SalesPoint>
+      </div>
+
+      <div className='footerContainer'>
+        <div className='container'>
+          <Contact></Contact>
+          <Footer></Footer>
+        </div>
       </div>
     </div>
   );
--- src/components/salesPoint/salesPoint.js
+++ src/components/salesPoint/salesPoint.js
@@ -1,11 +1,96 @@
 import React from 'react'
 import './salesPoint.css'
-
+import Button from '../button/button'
+import dev from '../../assets/images/dev_productivity.svg';
+import scrum from '../../assets/images/Scrum_board.svg';
+import check from '../../assets/images/check.png';
+import customer from '../../assets/images/business_deal (1).svg';
 
 
 function SalesPoint() {
   return (
-    <div>SalesPoint</div>
+    <div className='salesPointContainer'>
+      <div className='salesPoint'>
+        <div className='salesPointText'>
+          <h1>
+            We Build Technologies That Ensure Maximum Productivity
+          </h1>
+
+          <p>
+            Our engineers deliver solutions to improve work flow, efficiency, user engagement,
+            reduce costs and provide better customer experiences for our clients.
+          </p>
+
+          <Button buttonText={'Learn more'}></Button>
+
+        </div>
+
+        <div className='salesPointImg'>
+          <img src={dev} alt="Dev" />
+        </div>
+      </div>
+
+
+      <div className='salesPoint'>
+        <div className='salesPointImg'>
+          <img src={scrum} alt="Scrum" />
+        </div>
+
+        <div className='salesPointText'>
+          <h1>
+            Achieve Growth With Digital Solutions
+          </h1>
+
+          <p>
+            Our team of develeopers work with you to build user driven solutions that
+            are intuitive and allows easy user management.
+          </p>
+
+          <ul>
+            <li>
+              <img src={check} alt="Check" />
+              <p>
+                Support you across development lifecycle
+              </p>
+            </li>
+
+            <li>
+              <img src={check} alt="Check" />
+              <p>
+                Create your own skin to match your brand
+              </p>
+            </li>
+
+            <li>
+              <img src={check} alt="Check" />
+              <p>
+                Timely service
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className='salesPoint'>
+        <div className='salesPointText'>
+          <h1>
+            Customer Driven Solutions
+          </h1>
+
+          <p>
+            We are customer driven and prefer to work with our clients to understand their industry,
+            business and customers. From this discovery, we can recommend what solutions would best 
+            suit the business and its customers based on the functionality required, integration with
+            other business systems and budget available.
+          </p>
+        </div>
+
+        <div className='salesPointImg'>
+          <img src={customer} alt="Scrum" />
+        </div>
+      </div>
+
+    </div>
   )
 }
 
